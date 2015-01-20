f3e464b6cc022b4bc713dff6c8ffed7d04eb4172
Clemens <admin@cortys.de>
2015-01-20 19:56:26 +0100

    basic ui implementation
    no functionality

--- client/js/scanner.js
+++ client/js/scanner.js
@@ -30,9 +30,10 @@ var connector = (function() {
 			this.id = id;
 			this.title = title;
 			this.url = url;
+			this._current = {};
 
 			connector.listenFor(id, function(instance) {
-				that.currentInstance = instance;
+				that._current.instance = instance;
 				return that.api;
 			});
 		}
@@ -41,16 +42,47 @@ var connector = (function() {
 			id: null,
 			title: null,
 			url: null,
-			currentInstance: null,
+			_current: null,
 
 			open: function open() {
-				$.get(path+this.url, {}, function(data) {
+
+				// load style:
+				var style = document.createElement("link");
+				style.rel = "stylesheet";
+				style.type = "text/css";
+				style.href = path+this.url+"/style.css";
+
+				document.head.appendChild(style);
+				this._current.style = style;
+
+				// load script:
+				var script = document.createElement("script");
+				script.type = "text/javascript";
+				script.src = path+this.url+"/script.js";
+
+				document.head.appendChild(script);
+				this._current.script = script;
+
+				// load page:
+				$.get(path+this.url+"/index.html", {}, function(data) {
 					content.html(data);
 				}, "html");
+
 				return this;
 			},
 
-			close: function close() {
+			close: function close(callback) {
+				var that = this,
+					done = function() {
+						document.head.removeChild(that._current.script);
+						document.head.removeChild(that._current.style);
+						that._current.instance = that._current.script = that._current.style = null;
+						callback();
+					};
+				if(this._current.instance == null || typeof this._current.instance.close !== "function")
+					done();
+				else
+					this._current.instance.close(done);
 				return this;
 			},
 
@@ -75,10 +107,19 @@ var connector = (function() {
 				return true;
 			},
 			openPage: function openPage(id) {
+				var done = function() {
+					openedPage = pages[id].open();
+					window.history.pushState({ id:id }, openedPage.title, id);
+				};
+
 				if(openedPage instanceof Page)
-					openedPage.close();
-				openedPage = pages[id].open();
-				window.history.pushState({ id:id }, openedPage.title, id);
+					openedPage.close(done);
+				else
+					done();
+			},
+
+			get hasOpenedPage() {
+				return !!openedPage;
 			}
 		};
 	}());
@@ -91,6 +132,9 @@ var connector = (function() {
 			init: function init(menu) {
 				links = menu.find("a");
 
+				var startPage = window.location.pathname.substr(1),
+					firstId;
+
 				links.each(function(i) {
 					var element = $(this),
 						id = element.attr("href"),
@@ -98,7 +142,10 @@ var connector = (function() {
 						url = element.attr("data-url");
 					pages.addPage(id, title, url);
 
-					if(element.hasClass("active"))
+					if(!i)
+						firstId = id;
+
+					if(startPage == id)
 						pages.openPage(id);
 
 					element.bind("click", function(e) {
@@ -106,6 +153,10 @@ var connector = (function() {
 						e.preventDefault();
 					});
 				});
+
+				if(!pages.hasOpenedPage)
+					pages.openPage(firstId);
+
 			}
 		};
 	}());
--- client/pages/leihen/script.js
+++ client/pages/leihen/script.js
@@ -1,11 +1,95 @@
 (function() {
 
+	var listener;
+
 	var page = (function() {
 
+		function LendProcess() {
+
+			var that = this;
+
+			this.handleScan = function handleScan(word) {
+				word = word.substr(word.indexOf(" ")+1);
+
+				if(isNaN(word))
+					that.borrower = word;
+				else
+					that.item = word;
+			};
+
+			page.addScanListener(this.handleScan);
+		}
+		LendProcess.prototype = {
+			_borrower: null,
+			_item: null,
+			lender: null,
+
+			handleScan: null,
+
+			close: function close() {
+				page.removeScanListener(this.handleScan);
+			},
+
+			set borrower(value) {
+				this._borrower = value;
+			},
+			get borrower() {
+				return this._borrower;
+			},
+
+			set item(value) {
+				this._item = value;
+			},
+			get item() {
+				return this._item;
+			}
+		};
+
+		var main,
+			currentLendProcess,
+			currentScan = "",
+			scanListeners = new Set();
+
+		var page = {
+			init: function init(pMain) {
+				main = pMain;
+
+				currentLoanProcess = new LendProcess();
+
+				$(document).bind("keyup", listener = function(e) {
+					if(e.keyCode == 13) {
+						scanListeners.forEach(function(scanListener) {
+							scanListener(currentScan);
+						});
+						currentScan = "";
+						return;
+					}
+					currentScan += String.fromCharCode(e.keyCode);
+				});
+			},
+
+			addScanListener: function(listener) {
+				scanListeners.add(listener);
+			},
+			removeScanListener: function(listener) {
+				scanListeners.delete(listener);
+			}
+		};
+
+		return page;
 	}());
 
-	connector.checkIn("leihen", page, function(main) {
+	var api = (function() {
+		return {
+			close: function close(done) {
+				$(document).unbind("keyup", listener);
+				done();
+			}
+		};
+	}());
 
+	connector.checkIn("leihen", api, function(main) {
+		page.init(main);
 	});
 
 }());
