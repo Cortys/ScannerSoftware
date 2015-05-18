d4073198d79991626f3f7565f94b7ecbca8be2f2
Clemens Damke <admin@cortys.de>
2015-05-18 22:23:52 +0200

    added styling

--- client/pages/uebersicht/script.js
+++ client/pages/uebersicht/script.js
@@ -1,5 +1,22 @@
 (function() {
 
+	var filter = (function() {
+
+		var domFilters;
+
+		return {
+			init: function(pDomFilters) {
+				domFilters = pDomFilters;
+
+				domFilters.on("click", function() {
+					domFilters.filter(".active").removeClass("active");
+					$(this).addClass("active");
+					table.endedLendings = this.getAttribute("data-ended")*1;
+				});
+			}
+		};
+	}());
+
 	var table = (function() {
 
 		var domTable,
@@ -23,10 +40,18 @@
 				this.update();
 			},
 
+			get endedLendings() {
+				return request.endedLendings;
+			},
+
+			set endedLendings(value) {
+				request.endedLendings = !!value;
+				this.update();
+			},
+
 			update: function() {
 				$.post("/lendings", request, function(result) {
-					domTable.children("tr.entry").remove();
-
+					domTable.find("tr.entry").remove();
 					result.forEach(function(row, i) {
 						var rowHtml = "<tr class='entry'>";
 						headers.forEach(function(column) {
@@ -50,6 +75,7 @@
 
 	connector.checkIn("uebersicht", api, function(main) {
 		table.init($("#overview table"));
+		filter.init($("#filter button"));
 	});
 
 }());
